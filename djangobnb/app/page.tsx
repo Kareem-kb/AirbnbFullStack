ec097a1458cb63f6d5a5e40b5369c3f8081fc385
Kareem Bakarbashat <thought.kb@gmail.com>
2024-07-04 01:51:19 +0300

    Styling the NavBar

---
+++ djangobnb/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+     home page
+     <h2 className="text-airbnb"> Django project</h2>
+    </main>
+  );
+}
